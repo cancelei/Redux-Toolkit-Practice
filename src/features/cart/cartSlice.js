b3c54c5c5e9599ee659936446a22944769d8c2c1
Glauber <glauber.bannwart@gmail.com>
2023-07-26 14:08:38 -0300

    first commit

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,65 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
+
+const url = "https://course-api.com/react-useReducer-cart-project";
+
+const initialState = {
+  cartItems: cartItems,
+  amount: 5,
+  total: 0,
+  isLoading: true,
+};
+
+export const getCartItems = createAsyncThunk("cart/getCardItems", async () => {
+  return await fetch(url).then((response) => response.json().catch((error) => console.log(error)));
+});
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount++;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount--;
+    },
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.amount = amount;
+      state.total = total;
+    },
+  },
+  extraReducers: {
+    [getCartItems.pending]: (state, action) => {
+      state.isLoading = true;
+    }
+    ,
+    [getCartItems.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.cartItems = action.payload;
+    },
+    [getCartItems.rejected]: (state, action) => {
+      state.isLoading = false;
+    }
+  }
+});
+
+export const { clearCart, removeItem, increase, decrease, calculateTotals } =
+  cartSlice.actions;
+
+export default cartSlice.reducer;
